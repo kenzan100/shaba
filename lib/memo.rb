57748ca535bd7d0a8f7aef16b2d560cc972d4620
Yuta Okazaki <s04155yo@gmail.com>
2016-02-12 12:48:51 +0900

    memo got smarter by validating body

--- lib/memo.rb
+++ lib/memo.rb
@@ -1,6 +1,19 @@
 class Memo
+  class BodyMustBePresentError < ::StandardError; end
   attr_accessor :body
   def initialize(body_text)
-    @body = body_text
+    self.body = body_text
+  end
+
+  def body=(body_text)
+    organized_text = organize(body_text)
+    raise BodyMustBePresentError if organized_text.empty?
+    @body = organized_text
+  end
+
+  private
+
+  def organize(text)
+    text.chomp.strip
   end
 end
--- spec/memo_spec.rb
+++ spec/memo_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Memo do
 
   describe 'body attribute' do
     it { expect(subject.body).to eq 'hello' }
+
+    context 'when updating' do
+      before { subject.body = 'hello again' }
+      it { expect(subject.body).to eq 'hello again' }
+    end
+
+    context 'when blank' do
+      it { expect { subject.body = '' }.to raise_error(Memo::BodyMustBePresentError) }
+    end
   end
 end
