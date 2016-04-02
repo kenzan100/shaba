4860f7598550d3b1a6d8e2352ac984e43acccbad
Yuta Okazaki <s04155yo@gmail.com>
2016-04-02 22:46:20 +0200

    Introduce mini router upon Rack Request

--- apps/web/application.rb
+++ apps/web/application.rb
@@ -1,7 +1,25 @@
 module Web
   class Application
     def call(env)
-      [200, {}, ["Write New Memo"]]
+      method = env["REQUEST_METHOD"].downcase
+      path   = env["PATH_INFO"]
+      routes[method.to_sym][path.to_sym].call(env)
+    end
+
+    def routes
+      Router.routes
+    end
+  end
+
+  class Router
+    class << self
+      def routes
+        {
+          get: {
+            "/": ->(env) { [200, {}, ["Write New Memo"]] }
+          }
+        }
+      end
     end
   end
 end
