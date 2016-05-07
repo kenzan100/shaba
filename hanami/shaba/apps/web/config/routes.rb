a19db3a5fc786857f95cddcee210a0cba5cb9952
Yuta Okazaki <s04155yo@gmail.com>
2016-05-07 23:50:47 +0200

    hanami shaba have its first web controller / view

--- hanami/shaba/apps/web/config/routes.rb
+++ hanami/shaba/apps/web/config/routes.rb
@@ -1,2 +1,4 @@
 # Configure your routes here
 # See: http://www.rubydoc.info/gems/hanami-router/#Usage
+
+get '/', to: 'memos#new'
---
+++ hanami/shaba/apps/web/views/memos/new.rb
@@ -0,0 +1,5 @@
+module Web::Views::Memos
+  class New
+    include Web::View
+  end
+end
