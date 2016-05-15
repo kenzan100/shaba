4711c6c5db2d49eff9097f57e56ac77197e4dd6b
Yuta Okazaki <s04155yo@gmail.com>
2016-05-15 15:37:40 +0200

    Add navigation

--- hanami/shaba/apps/web/views/application_layout.rb
+++ hanami/shaba/apps/web/views/application_layout.rb
@@ -2,6 +2,14 @@ module Web
   module Views
     class ApplicationLayout
       include Web::Layout
+
+      def navigation_links
+        [
+          link_to('Write New', routes.new_memo_path),
+          link_to('Memos', routes.memos_path),
+          link_to('Posts', routes.space_path(:posts))
+        ]
+      end
     end
   end
 end
